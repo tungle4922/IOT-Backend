0f38ce51c372175f464e805880a85aa0db39f4b1
Tùng <letung49222@gmail.com>
2024-03-20 23:26:37 +0700

    add package

--- database.js
+++ database.js
@@ -1,11 +1,18 @@
-const mysql = require('mysql2/promise')
+const mysql = require("mysql2/promise");
 
 const mysqlPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Tung4922@',
-    database: 'iot_exam'
-})
+  host: "localhost",
+  user: "root",
+  password: "Tung4922@",
+  database: "iot_exam",
+});
 
+mysqlPool
+  .query("SELECT 1")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(console.log("error"))
+  .then(() => mysqlPool.end());
 
-module.exports = mysqlPool
+module.exports = mysqlPool;
---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require("express"),
+  app = express(),
+  bodyparser = require("body-parser");
+require("express-async-errors");
